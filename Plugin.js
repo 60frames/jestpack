8ff381349000318d4461f8810ca656f260209de5
Richard <riscarrott@googlemail.com>
2015-11-08 17:04:03 +0000

    Convert Plugin to es6

--- Plugin.js
+++ Plugin.js
@@ -1,13 +1,6 @@
 'use strict';
 
-var ConstDependency = require('webpack/lib/dependencies/ConstDependency');
-
-/**
- * Adds support for Jest API.
- * e.g. jest.dontMock('./foo') => jest.dontMock(1);
- * @constructor
- */
-function JestpackPlugin() {}
+const ConstDependency = require('webpack/lib/dependencies/ConstDependency');
 
 /**
  * Resolves module paths.
@@ -26,51 +19,57 @@ function resolveArgument(expr, argIndex) {
 }
 
 /**
- * Resolves Jest API `moduleName` arguments and updates the Webpack runtime.
- * @return {Undefined} undefined.
+ * Adds support for Jest API.
+ * e.g. jest.dontMock('./foo') => jest.dontMock(1);
+ * @constructor
  */
-JestpackPlugin.prototype.apply = function(compiler) {
+class JestpackPlugin {
 
-    compiler.parser.plugin('call jest.dontMock', resolveArgument);
-    compiler.parser.plugin('call jest.mock', resolveArgument);
-    compiler.parser.plugin('call jest.genMockFromModule', resolveArgument);
-    compiler.parser.plugin('call jest.setMock', resolveArgument);
-    compiler.parser.plugin('call jest._registerManualMock', function(expr) {
-        resolveArgument.call(this, expr);
-        resolveArgument.call(this, expr, 1);
-    });
-    compiler.parser.plugin('call require.requireActual', resolveArgument);
-    compiler.parser.plugin('expression require.requireActual', function(expr) {
-        var dep = new ConstDependency('__webpack_require__.requireActual', expr.range);
-        dep.loc = expr.loc;
-        this.state.current.addDependency(dep);
-        return true;
-    });
+    /**
+     * Resolves Jest API `moduleName` arguments and updates the Webpack runtime.
+     * @param  {Object} compiler The Webpack compiler.
+     */
+    apply(compiler) {
 
-    compiler.plugin('compilation', function(compilation) {
-
-        // Replace `__webpack_require__` with `require` as defined by Jest's JSDom env.
-        compilation.mainTemplate.plugin('module-require', function() {
-            return 'jest._webpackRequire';
+        compiler.parser.plugin('call jest.dontMock', resolveArgument);
+        compiler.parser.plugin('call jest.mock', resolveArgument);
+        compiler.parser.plugin('call jest.genMockFromModule', resolveArgument);
+        compiler.parser.plugin('call jest.setMock', resolveArgument);
+        compiler.parser.plugin('call jest._registerManualMock', function(expr) {
+            resolveArgument.call(this, expr);
+            resolveArgument.call(this, expr, 1);
+        });
+        compiler.parser.plugin('call require.requireActual', resolveArgument);
+        compiler.parser.plugin('expression require.requireActual', function(expr) {
+            var dep = new ConstDependency('__webpack_require__.requireActual', expr.range);
+            dep.loc = expr.loc;
+            this.state.current.addDependency(dep);
+            return true;
         });
 
-        // Make all references to the module cache global so 'jestpack/ModuleLoader'
-        // can manage the object *and the reference*.
-        compilation.mainTemplate.plugin('local-vars', function(source) {
-            return source.replace('var installedModules', 'window.installedModules');
+        compiler.plugin('compilation', compilation => {
+
+            // Replace `__webpack_require__` with `require` as defined by Jest's JSDom env.
+            compilation.mainTemplate.plugin('module-require', () => {
+                return 'jest._webpackRequire';
+            });
+
+            // Make all references to the module cache global so 'jestpack/ModuleLoader'
+            // can manage the object *and the reference*.
+            compilation.mainTemplate.plugin('local-vars', source => {
+                return source.replace('var installedModules', 'window.installedModules');
+            });
+
+            // Provide `__webpack_require__` to 'jestpack/ModuleLoader'.
+            compilation.mainTemplate.plugin('startup', source => {
+                return [
+                    '',
+                    'jest._setupWebpackRequire(__webpack_require__);',
+                    source
+                ].join('\n');
+            });
         });
-
-        // Expose `__webpack_require__` for 'jestpack/ModuleLoader'.
-        compilation.mainTemplate.plugin('startup', function(source) {
-            return [
-                '',
-                'jest._setupWebpackRequire(__webpack_require__);',
-                source
-            ].join('\n');
-        });
-
-    });
-
-};
+    }
+}
 
 module.exports = JestpackPlugin;
